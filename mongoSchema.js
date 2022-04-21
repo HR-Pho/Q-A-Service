5a66d2bf93a58a0d646503a5784424a1be40d899
Matthew Luu <mattmluu@gmail.com>
2022-04-20 22:58:30 -0700

    mango schema using mongoose & eslint js file

---
+++ mongoSchema.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+/* ---- SCHEMA TYPES ----
+String
+Number
+Date
+Buffer
+Boolean
+Mixed
+ObjectId
+Array
+Decimal128
+Map
+*/
+
+//mongoose auto creates ids
+const qaSchema = new Schema({
+  productId: Number,
+  questionBody:  String,
+  questionAuthor: String,
+  questionCreation: String,
+  questionReported: Boolean,
+  questionHelpfulness: Number,
+  answers: [{
+    questionId: Number,
+    answerBody:  String,
+    answerAuthor: String,
+    answerCreation: String,
+    answerReported: Boolean,
+    answerHelpfulness: Number,
+    answerPhotos: [{
+      answerId: Number,
+      url: String,
+    }]
+  }]
+});
+
+const QA = mongoose.model('QA', qaSchema);
+sadsdasad
