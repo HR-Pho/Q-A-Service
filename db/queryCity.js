038cd3a0605c56eb8950349668d584a1f107edae
Matthew Luu <mattmluu@gmail.com>
2022-04-25 13:58:42 -0700

    getAnswers & getQuestions done

--- db/queryCity.js
+++ db/queryCity.js
@@ -7,7 +7,6 @@ module.exports = {
       product_id: product_id,
       results: []
     };
-
     pool
       .query(`SELECT * FROM questions WHERE product_id = ${product_id}`)
       .then(result => {
@@ -28,10 +27,37 @@ module.exports = {
       })
       .catch(err => res.status(500).send(err));
   },
+
   postQuestion: '',
   markQuestionHelpful: '',
   reportQuestion: '',
-  getAnswers: '',
+  getAnswers: (req, res) => {
+    let { count = 2, page = 1 } = req.query;
+    const { question_id } = req.params;
+    const payload = {
+      question_id: req.params.question_id,
+      results: []
+    }
+    pool
+      .query(`SELECT * FROM answers WHERE question_id = ${question_id}`)
+      .then(result => {
+        if (count > result.rows.length) { count = result.rows.length }
+        for (let i = 0; i < count; i++) {
+          let { id, body, date_written, answerer_name, helpful } = result.rows[i];
+          payload.results.push({
+            answer_id: id,
+            body: body,
+            date: date_written,
+            answerer_name: answerer_name,
+            helpfulness: helpful,
+            photos: []
+          });
+        }
+        res.status(200).send(payload);
+      })
+      .catch(err => res.status(500).send(err));
+  },
+
   postAnswer: '',
   markAnswerHelpful: '',
   reportAnswer: ''
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { pool } = require('../db/db.js');
 const express = require('express');
-const { getQuestions } = require('../db/queryCity.js');
+const { getQuestions, getAnswers } = require('../db/queryCity.js');
 
 const app = express();
 app.use(express.json());
@@ -19,46 +19,9 @@ app.get('/TEST/:varyabowl/POSTMAN', (req, res) => {
   res.send('joe mama')
 });
 
-/*---------------------------------------------------------------------------------*/
-// LIST OF QUESTIONS:
-// REQUEST FORMAT -- GET /qa/questions
 app.get('/qa/questions', getQuestions);
 
-/*---------------------------------------------------------------------------------*/
-// LIST OF ANSWERS:
-// REQUEST FORMAT --  GET /qa/questions/:question_id/answers
-//postman test --- localhost:3000/qa/questions/573875/answers
-app.get('/qa/questions/:question_id/answers', (req, res) => {
-  const payload = {
-    question_id: req.params.question_id,
-    results: []
-  }
-  let count = req.query.count || 5
-  const page = req.query.page || 1
-
-  // console.log(`req.query.product_id:: ${req.params.question_id}`)
-  // console.log(`req.query.count:: ${req.query.count}`)
-  // console.log(`req.query.page:: ${req.query.page}`)
-
-  pool
-    .query(`SELECT * FROM answers WHERE question_id = ${req.params.question_id}`)
-    .then(result => {
-      const answerBlueprint = {};
-      if (count > result.rows.length) { count = result.rows.length }
-      for (let i = 0; i < count; i++) {
-        let row = result.rows[i];
-        answerBlueprint.answer_id = row.id;
-        answerBlueprint.body = row.body;
-        answerBlueprint.date = row.date_written;
-        answerBlueprint.answerer_name = row.answerer_name;
-        answerBlueprint.helpfulness = row.helpful;
-        answerBlueprint.photos = [];
-        payload.results.push(answerBlueprint);
-      }
-      res.status(200).send(payload);
-    })
-    .catch(err => res.status(500).send(err));
-});
+app.get('/qa/questions/:question_id/answers', getAnswers);
 
 /*---------------------------------------------------------------------------------*/
 // ADDS A QUESTIONS:
