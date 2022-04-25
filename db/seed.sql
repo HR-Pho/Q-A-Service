3ccc2dc689f9040117c6bfc063b47cf891f25788
Matthew Luu <mattmluu@gmail.com>
2022-04-25 08:12:29 -0700

    added DS_Store to gitignore

--- db/seed.sql
+++ db/seed.sql
@@ -1,14 +1,14 @@
 COPY questions(id, product_id, body, date_written, asker_name, asker_email, reported, helpful)
-FROM '/Users/matthewluu/HackReactor/RFP2202/Q-A-Service/csvData/questions.csv'
+FROM '/Users/matthewluu/HackReactor/csvData/questions.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY answers(id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful)
-FROM '/Users/matthewluu/HackReactor/RFP2202/Q-A-Service/csvData/answers.csv'
+FROM '/Users/matthewluu/HackReactor/csvData/answers.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY photos(id, answer_id, url)
-FROM '/Users/matthewluu/HackReactor/RFP2202/Q-A-Service/csvData/answers_photos.csv'
+FROM '/Users/matthewluu/HackReactor/csvData/answers_photos.csv'
 DELIMITER ','
 CSV HEADER;
