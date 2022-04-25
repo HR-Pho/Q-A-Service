24d9090f221f6beda5fff3a8b894a0a89251a0aa
Matthew Luu <mattmluu@gmail.com>
2022-04-25 08:41:45 -0700

    server set up and outline

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-//API
+require('dotenv').config();
+const db = require('./db/db.js');
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log(`QA server listening on port:'${process.env.PORT}`)
+});
+
+
+/* needed routes:
+
+LIST OF QUESTIONS:
+GET /qa/questions
+
+LIST OF ANSWERS:
+GET /qa/questions/:question_id/answers
+
+ADDS A QUESTIONS:
+POST /qa/questions
+
+ADDS AN ANSWER:
+POST /qa/questions/:question_id/answers
+
+MARK QUESTIONS AS HELPFUL:
+PUT /qa/questions/:question_id/helpful
+
+MARK ANSWER AS HELPFUL:
+PUT /qa/answers/:answer_id/helpful
+
+REPORT QUESTION:
+PUT /qa/questions/:question_id/report
+
+REPORT ANSWER:
+PUT /qa/answers/:answer_id/report
+
+*/
