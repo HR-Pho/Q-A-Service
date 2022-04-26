c1b573e37d433b7e3e27ef3cbd7bdab761849f27
Matthew Luu <mattmluu@gmail.com>
2022-04-25 23:36:36 -0700

    got rid of test route on server

--- server/index.js
+++ server/index.js
@@ -9,16 +9,6 @@ app.listen(process.env.PORT, () => {
   console.log(`QA server listening on port:'${process.env.PORT}`)
 });
 
-
-app.get('/TEST/:varyabowl/POSTMAN', (req, res) => {
-  console.log(req.params.varyabowl);
-  if (req.query) {
-    console.log(`REQUEST QUERY PARAMS: ${req.query.key} <-- SHOULD = VALUE `);
-  }
-  console.log('Knock knock... who is there?');
-  res.send('joe mama')
-});
-
 app.get('/qa/questions', getQuestions);
 app.get('/qa/questions/:question_id/answers', getAnswers);
 
