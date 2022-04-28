6c36fb02b846d9218cd640a2c2493ed87f9ec1f6
Matthew Luu <mattmluu@gmail.com>
2022-04-28 03:19:56 -0700

    added indexing and proper dates

--- db/seed.sql
+++ db/seed.sql
@@ -3,15 +3,25 @@ FROM '/Users/matthewluu/HackReactor/csvData/questions.csv'
 DELIMITER ','
 CSV HEADER;
 ALTER SEQUENCE questions_id_seq RESTART WITH 3518964;
+UPDATE questions SET date_written=date_written/1000;
+ALTER TABLE questions ALTER COLUMN date_written TYPE timestamp without time zone using to_timestamp(date_written) AT TIME ZONE 'UTC';
+ALTER TABLE questions ALTER COLUMN date_written SET DEFAULT NOW();
+CREATE INDEX questionsIndex ON questions (product_id);
+
 
 COPY answers(id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful)
 FROM '/Users/matthewluu/HackReactor/csvData/answers.csv'
 DELIMITER ','
 CSV HEADER;
 ALTER SEQUENCE answers_id_seq RESTART WITH 6879307;
+UPDATE answers SET date_written=date_written/1000;
+ALTER TABLE answers ALTER COLUMN date_written TYPE timestamp without time zone using to_timestamp(date_written) AT TIME ZONE 'UTC';
+ALTER TABLE answers ALTER COLUMN date_written SET DEFAULT NOW();
+CREATE INDEX answersIndex ON answers (question_id);
 
 COPY photos(id, answer_id, url)
 FROM '/Users/matthewluu/HackReactor/csvData/answers_photos.csv'
 DELIMITER ','
 CSV HEADER;
 ALTER SEQUENCE photos_id_seq RESTART WITH 2063760;
+CREATE INDEX photosIndex ON photos (answer_id);
